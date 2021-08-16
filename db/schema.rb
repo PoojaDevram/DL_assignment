5d6fac2d157b9da2b0d0486d27faebca1183c6c9
Pooja Devram <poojadevram@gmail.com>
2021-08-16 15:19:56 +0530

    Added Book Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_093233) do
+ActiveRecord::Schema.define(version: 2021_08_16_094241) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.date "date_of_birth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "book_title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
