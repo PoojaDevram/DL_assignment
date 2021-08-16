d97c8ead63a7df363b4d3ce0a4b4876f7868a6fd
Pooja Devram <poojadevram@gmail.com>
2021-08-16 15:09:08 +0530

    Added author model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_091028) do
+ActiveRecord::Schema.define(version: 2021_08_16_093233) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
